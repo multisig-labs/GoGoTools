468dd24fcf9a76d1f0e5268e7a8eebbcd4ee0dfd
Johnny Gault <johnnygault@protonmail.com>
2023-01-26 12:52:29 -0800

    Docs

--- main.go
+++ main.go
@@ -18,7 +18,9 @@ func panicHandler() {
 	if panicPayload := recover(); panicPayload != nil {
 		stack := string(debug.Stack())
 		fmt.Fprintln(os.Stderr, "================================================================================")
-		fmt.Fprintln(os.Stderr, "            Fatal error. This is a bug!")
+		fmt.Fprintln(os.Stderr, "            Fatal error. Sorry! You found a bug.")
+		fmt.Fprintln(os.Stderr, "    Please copy all of this info into an issue at")
+		fmt.Fprintln(os.Stderr, "     https://github.com/multisig-labs/gogotools")
 		fmt.Fprintln(os.Stderr, "================================================================================")
 		fmt.Fprintf(os.Stderr, "Version:           %s\n", version.Version)
 		fmt.Fprintf(os.Stderr, "Build Date:        %s\n", version.BuildDate)
