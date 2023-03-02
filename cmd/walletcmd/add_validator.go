aa214cbb1419936df06e2c2e0bfc05dbc95fd135
Johnny Gault <johnnygault@protonmail.com>
2023-03-02 13:19:04 -0800

    Add utils mnemonic

---
+++ cmd/walletcmd/add_validator.go
@@ -0,0 +1,82 @@
+package walletcmd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/crypto"
+	"github.com/ava-labs/avalanchego/utils/units"
+	"github.com/ava-labs/avalanchego/vms/platformvm/validator"
+	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
+	"github.com/ava-labs/avalanchego/wallet/subnet/primary"
+	"github.com/multisig-labs/gogotools/pkg/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// TODO This doesnt seem to work on a --staking-disabled node, so maybe not worth including?
+
+func newAddValidatorCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add-validator work-dir",
+		Short: "Issue a AddValidator tx and return the txID",
+		Long:  ``,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if exists := utils.DirExists(args[0]); !exists {
+				return fmt.Errorf("node directory does not exist: %s", args[0])
+			}
+			key, err := decodePrivateKey(viper.GetString("pk"))
+			cobra.CheckErr(err)
+			txID, err := addValidator(key)
+			cobra.CheckErr(err)
+			fmt.Println(txID)
+			return nil
+		},
+	}
+	return cmd
+}
+
+func addValidator(key *crypto.PrivateKeySECP256K1R) (ids.ID, error) {
+	uri := viper.GetString("node-url")
+	kc := secp256k1fx.NewKeychain(key)
+	subnetOwner := key.Address()
+	ctx := context.Background()
+
+	wallet, err := primary.NewWalletFromURI(ctx, uri, kc)
+	if err != nil {
+		return ids.Empty, fmt.Errorf("failed to initialize wallet: %w", err)
+	}
+
+	owner := &secp256k1fx.OutputOwners{
+		Threshold: 1,
+		Addrs: []ids.ShortID{
+			subnetOwner,
+		},
+	}
+
+	nodeID := "NodeID-5FKRwqFyQnZGoN7FTc3t3TUHyuUGPhBxJ"
+	nodeShortID, err := ids.NodeIDFromString(nodeID)
+	if err != nil {
+		return ids.ID{}, fmt.Errorf("error decoding nodeID %s: %w", nodeID, err)
+	}
+
+	startTime := time.Now().Add(10 * time.Second)
+	endTime := startTime.Add(24 * time.Hour)
+
+	vdr := validator.Validator{
+		NodeID: nodeShortID,
+		Start:  uint64(startTime.Unix()),
+		End:    uint64(endTime.Unix()),
+		Wght:   2 * units.KiloAvax,
+	}
+
+	txID, err := wallet.P().IssueAddValidatorTx(&vdr, owner, 20000)
+	if err != nil {
+		return ids.Empty, fmt.Errorf("failed to issue AddValidatorTx: %w", err)
+	}
+
+	return txID, nil
+}
