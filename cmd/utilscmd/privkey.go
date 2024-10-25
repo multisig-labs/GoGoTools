140150f2ec4ebffc6b4499a7b0bc2f0c15a5a7cd
Johnny Gault <john@multisiglabs.org>
2024-10-25 14:13:50 -0700

    Also handle hex-encoded private keys

--- cmd/utilscmd/privkey.go
+++ cmd/utilscmd/privkey.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ava-labs/avalanchego/utils/cb58"
 	"github.com/ava-labs/avalanchego/utils/crypto/secp256k1"
 	"github.com/ava-labs/avalanchego/utils/formatting/address"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/spf13/cobra"
 )
 
@@ -17,10 +18,23 @@ func newPrivkeyCmd() *cobra.Command {
 		Long:  ``,
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			pk, _ := strings.CutPrefix(args[0], "PrivateKey-")
-			pkBytes, err := cb58.Decode(pk)
-			cobra.CheckErr(err)
-			fmt.Printf("PrivKey Bytes: %#x\n", pkBytes)
+			var pkBytes []byte
+			var err error
+
+			if strings.HasPrefix(args[0], "0x") {
+				// Decode hex input
+				pkBytes, err = hexutil.Decode(args[0])
+				cobra.CheckErr(err)
+			} else {
+				// Existing CB58 decoding
+				pk, _ := strings.CutPrefix(args[0], "PrivateKey-")
+				pkBytes, err = cb58.Decode(pk)
+				cobra.CheckErr(err)
+			}
+
+			pkcb, _ := cb58.Encode(pkBytes)
+			fmt.Printf("PrivKey Hex: %#x\n", pkBytes)
+			fmt.Printf("PrivKey CB58: PrivateKey-%s\n", pkcb)
 
 			secpk, err := secp256k1.ToPrivateKey(pkBytes)
 			cobra.CheckErr(err)
